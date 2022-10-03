10cee8f85902c41eece1d0398c211daa49d0c52f
Tamino <tamino.mende@gmail.com>
2022-10-03 17:37:40 +0200

    Search Bar Version 1

--- lib/pages/roomplan_page.dart
+++ lib/pages/roomplan_page.dart
@@ -14,10 +14,76 @@ class RoomPlan extends StatelessWidget {
       child: Scaffold(
         appBar: MCGAppBar(title: "MCG App"),
         drawer: const MCGDrawer(),
-        body: const Center(
-            child: Text("Raumplan")
+        body: Column(
+          children: <Widget> [
+            Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(32.0, 32.0, 32.0, 24.0),
+              child: Row(children: <Widget> [
+                IconButton(onPressed: () {
+                  showSearch(context: context,
+                      delegate: MySearchDelegate());
+                }, icon: const Icon(Icons.search, size: 40,)),
+                const Text('  Search', style: const TextStyle(fontSize: 32),),
+              ],)
+            )
+          ],
+        )
         ),
-      ),
+      );
+  }
+}
+
+class MySearchDelegate extends SearchDelegate {
+  @override
+  Widget? buildLeading(BuildContext context) => IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () => close(context, null),
+      );
+
+  @override
+  List<Widget>? buildActions(BuildContext context) => [
+    IconButton(
+      icon: const Icon(Icons.clear),
+      onPressed: () {
+        if (query.isEmpty) {
+          close(context, null);
+        } else {
+          query = '';
+        }
+      },
+    ),
+  ];
+
+  @override
+  Widget buildResults(BuildContext context) => Center(
+    child: Text(
+      query,
+      style: const TextStyle(fontSize: 64)
+    ),
+  );
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List <String> suggestions = [
+      'Raum 1.64',
+      'Raum 1.65',
+      'Raum 1.66'
+    ];
+
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (context, index) {
+        final suggestion = suggestions[index];
+
+        return ListTile(
+          title: Text(suggestion),
+          onTap:() {
+            query = suggestion;
+
+            showResults(context);
+          },
+        );
+        },
     );
   }
 }
