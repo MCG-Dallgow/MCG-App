87d8374c1d39d752389c6cdc77c344541e3f0926
SLH335 <114884726+SLH335@users.noreply.github.com>
2022-10-03 15:39:13 +0200

    Update base structure

--- lib/pages/timetable_page.dart
+++ lib/pages/timetable_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:mcgapp/widgets/app_bar.dart';
 import 'package:mcgapp/widgets/drawer.dart';
 
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mcgapp/pages/roomplan_page.dart';
+import 'package:mcgapp/pages/substitution_page.dart';
 
 import '../pages/home_page.dart';
 import '../pages/timetable_page.dart';
@@ -51,12 +53,26 @@ class MCGDrawer extends StatelessWidget {
           ListTile(
             leading: const Icon(Icons.calendar_today),
             title: const Text('Vertretungsplan'),
-            onTap: () { },
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return const SubstitutionPlan();
+                }),
+              );
+            },
           ),
           ListTile(
             leading: const Icon(Icons.room_outlined),
             title: const Text('Raumplan'),
-            onTap: () { },
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return const RoomPlan();
+                }),
+              );
+            },
           ),
         ],
       ),
