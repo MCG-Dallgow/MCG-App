0281b7d513852682763344d341d7dd0431177e4a
SLH335 <114884726+SLH335@users.noreply.github.com>
2022-10-04 08:48:48 +0200

    Typo
    Signed-off-by: SLH335 <114884726+SLH335@users.noreply.github.com>

--- lib/pages/roomplan_page.dart
+++ lib/pages/roomplan_page.dart
@@ -95,7 +95,7 @@ class MySearchDelegate extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    List<String> sugestions = searchResults.where((searchResult) {
+    List<String> suggestions = searchResults.where((searchResult) {
       final result = searchResult.toLowerCase();
       final input = query.toLowerCase();
 
