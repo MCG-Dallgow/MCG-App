f570b96ae2ff8476153703dbb6fe3a2b490e3eb4
SLH335 <114884726+SLH335@users.noreply.github.com>
2022-10-04 15:13:41 +0200

    Teacher List v2
    Signed-off-by: SLH335 <114884726+SLH335@users.noreply.github.com>

--- lib/pages/teachers_page.dart
+++ lib/pages/teachers_page.dart
@@ -1,15 +1,40 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:mcgapp/widgets/app_bar.dart';
 import 'package:mcgapp/widgets/drawer.dart';
 
-class TeachersPage extends StatelessWidget {
-  TeachersPage({Key? key}) : super(key: key);
+class TeachersPage extends StatefulWidget {
+  const TeachersPage({Key? key}) : super(key: key);
 
+  @override
+  State<TeachersPage> createState() => _TeachersPageState();
+}
+
+class _TeachersPageState extends State<TeachersPage> {
+  /*
   final List<String> teachers = <String>[
     "Herr Möller",
     "Herr Sydow",
     "Frau Gatz"
   ];
+  */
+  List _teachers = [];
+
+  Future<String> loadJsonData() async {
+    var jsonText = await rootBundle.loadString("assets/teachers.json");
+    setState(() {
+      _teachers = json.decode(jsonText)["teachers"];
+    });
+    return "success";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadJsonData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,23 +47,25 @@ class TeachersPage extends StatelessWidget {
           actions: [
             IconButton(
               icon: const Icon(Icons.search),
-              onPressed: () { },
+              onPressed: () {  },
             ),
           ],
         ),
         drawer: const MCGDrawer(),
-        body: ListView.builder(
-            padding: const EdgeInsets.all(8),
-            itemCount: teachers.length * 2,
-            itemBuilder: (BuildContext context, int index) {
-              if (index.isOdd) {
-                return const Divider();
-              }
-              return ListTile(
-                title: Text(teachers[index ~/ 2]),
-                onTap: () {  },
-              );
-            }),
+        body: _teachers.isNotEmpty
+            ? ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: _teachers.length * 2,
+              itemBuilder: (BuildContext context, int index) {
+                if (index.isOdd) {
+                  return const Divider();
+                }
+                return ListTile(
+                  title: Text("${_teachers[index ~/ 2]["anrede"]} ${_teachers[index ~/ 2]["nachname"]}"),
+                  onTap: () {  },
+                );
+              })
+          : const Center(child: Text("empty"))
       ),
     );
   }
