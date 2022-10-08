52dd023ab256107f59a10bebfd1a0e2d92369c55
SLH335 <114884726+SLH335@users.noreply.github.com>
2022-10-08 10:56:34 +0200

    Substitution plan as WebView

--- lib/screens/substitutions_screen.dart
+++ lib/screens/substitutions_screen.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 import '../widgets/app_bar.dart';
 import '../widgets/drawer.dart';
 
 class SubstitutionsScreen extends StatefulWidget {
-  const SubstitutionsScreen({Key? key}) : super(key: key);
+  const SubstitutionsScreen({Key? key})
+      : super(key: key);
 
   @override
   State<SubstitutionsScreen> createState() => _SubstitutionsScreenState();
 }
 
 class _SubstitutionsScreenState extends State<SubstitutionsScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -27,22 +28,51 @@ class _SubstitutionsScreenState extends State<SubstitutionsScreen> {
           title: "Vertretungsplan",
           actions: [
             IconButton(
-              icon: const Icon(Icons.filter_alt),
+              icon: const Icon(Icons.refresh),
               onPressed: () {
-                //connect();
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          const SubstitutionsScreen()),
+                  (Route<dynamic> route) => false,
+                );
               },
+            ),
+            IconButton(
+              icon: const Icon(Icons.filter_alt),
+              onPressed: () { },
+            ),
+          ],
+          bottom: const TabBar(
+            physics: NeverScrollableScrollPhysics(),
+            tabs: [
+              Tab(
+                child: Text("Heute"),
+              ),
+              Tab(
+                child: Text("Morgen"),
+              ),
+            ],
+          ),
+        ),
+        drawer: const MCGDrawer(),
+        body: TabBarView(
+          children: [
+            InteractiveViewer(
+              panEnabled: false,
+              child: const WebView(
+                initialUrl: 'https://dsbmobile.de/data/b89afef4-8a32-4480-bef8-1c874b1d8e62/dd3fb991-2339-4fce-8a1b-afb649e2cfa5/subst_001.htm'
+                ),
+            ),
+            InteractiveViewer(
+              panEnabled: false,
+              child: const WebView(
+                initialUrl: 'https://dsbmobile.de/data/b89afef4-8a32-4480-bef8-1c874b1d8e62/dd3fb991-2339-4fce-8a1b-afb649e2cfa5/subst_002.htm',
+              ),
             ),
           ],
         ),
-        drawer: const MCGDrawer(),
-        body: InteractiveViewer(
-          maxScale: 4,
-          child: Container(
-            alignment: Alignment.topCenter,
-            constraints: const BoxConstraints.expand(),
-            child: Image.asset('assets/images/substitution_plan.jpg'),
-          )
-        )
       ),
     );
   }
