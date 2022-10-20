ab18841570b90a7ef9d32b8e83b8a934d9b7602f
SLH335 <114884726+SLH335@users.noreply.github.com>
2022-10-21 00:05:10 +0200

    Reworked substitution plan with WebUntis

---
+++ lib/classes/substitution_entry.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:html/parser.dart';
+
+class SubstitutionEntry extends StatelessWidget {
+  const SubstitutionEntry({
+    Key? key,
+    required this.group,
+    required this.lesson,
+    required this.times,
+    this.teacherOld,
+    this.teacherNew,
+    this.roomOld,
+    this.roomNew,
+    this.courseOld,
+    this.courseNew,
+    this.type,
+    this.description,
+  }) : super(key: key);
+
+  final String group; // Klasse(nstufe), auf die die Änderung zutrifft
+  final String lesson; // Schulstunde ("3-4" -> 2. Block)
+  final String times; // Zeiten der Stunde (z.B. 11:30-13:00)
+  final String? teacherOld; // regulärer Lehrer
+  final String? teacherNew; // Vertretungslehrer
+  final String? roomOld; // regulärer Raum
+  final String? roomNew; // Raumänderung
+  final String? courseOld; // reguläre(r/s) Fach/Kurs
+  final String? courseNew; // Fach-/Kursänderung
+  final String? type; // Art der Änderung (Entfall, Raumänderung usw.)
+  final String? description; // Vertretungstext
+
+  static String _parseHtmlString(String htmlString) {
+    final document = parse(htmlString);
+    final String parsedString = (parse(document.body?.text).documentElement?.text ?? htmlString)
+        .replaceAll('&auml;', 'ä')
+        .replaceAll('&ouml;', 'ö')
+        .replaceAll('&uuml;', 'ü');
+    return parsedString;
+  }
+
+  factory SubstitutionEntry.fromJson(var data) {
+    RegExp old = RegExp(r'\([a-zA-Z0-9.]{0,20}\)');
+    String group = data['group'];
+    String lesson = data['data'][0].toString().replaceAll(' ', '');
+    String times = data['data'][1];
+
+    RegExpMatch? courseMatch = old.firstMatch(data['data'][3]);
+    String courseOld = courseMatch != null ? courseMatch[0] ?? '' : '';
+    String courseNew = (data['data'][3] as String).replaceAll(courseOld, '');
+
+    RegExpMatch? roomMatch = old.firstMatch(_parseHtmlString(data['data'][4]));
+    String roomOld = roomMatch != null ? roomMatch[0] ?? '' : '';
+    String roomNew = _parseHtmlString(data['data'][4]).replaceAll(roomOld, '');
+
+    RegExpMatch? teacherMatch = old.firstMatch(_parseHtmlString(data['data'][5]));
+    String teacherOld = teacherMatch != null ? teacherMatch[0] ?? '' : '';
+    String teacherNew = _parseHtmlString(data['data'][5]).replaceAll(teacherOld, '');
+
+    String type = _parseHtmlString(data['data'][6]);
+    String description = data['data'][7];
+    return SubstitutionEntry(
+        group: group,
+        lesson: lesson,
+        times: times,
+        teacherNew: teacherNew,
+        teacherOld: teacherOld,
+        roomNew: roomNew,
+        roomOld: roomOld,
+        courseNew: courseNew,
+        courseOld: courseOld,
+        type: type,
+        description: description);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const double fontSize = 16;
+    bool isCancelled = type == 'Entfall';
+    TextStyle headerStyle = const TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: fontSize,
+    );
+    TextStyle normalStyle = const TextStyle(
+      fontSize: fontSize,
+    );
+
+    return Container(
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(5.0), color: Colors.teal),
+      padding: const EdgeInsets.all(10.0),
+      margin: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(group, style: headerStyle),
+              Text(type ?? '', style: headerStyle),
+              Text(lesson, style: headerStyle),
+            ],
+          ),
+          const Divider(color: Colors.black),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Text(teacherNew ?? '', style: TextStyle(
+                    decoration: isCancelled ? TextDecoration.lineThrough : null,
+                    fontWeight: teacherOld != '' ? FontWeight.bold : null,
+                    fontSize: fontSize,
+                  )),
+                  Text(teacherOld ?? '', style: normalStyle),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(roomNew ?? '', style: TextStyle(
+                    decoration: isCancelled ? TextDecoration.lineThrough : null,
+                    fontWeight: roomOld != '' ? FontWeight.bold : null,
+                    fontSize: fontSize,
+                  )),
+                  Text(roomOld ?? '', style: normalStyle),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(courseNew ?? '', style: TextStyle(
+                    decoration: isCancelled ? TextDecoration.lineThrough : null,
+                    fontWeight: courseOld != '' ? FontWeight.bold : null,
+                    fontSize: fontSize,
+                  )),
+                  Text(courseOld ?? '', style: normalStyle),
+                ],
+              ),
+            ],
+          ),
+          const Divider(color: Colors.black),
+          Text(description ?? '', style: normalStyle),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/substitutions_screen.dart
+++ lib/screens/substitutions_screen.dart
@@ -1,47 +1,115 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:dsb/dsb.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:webviewx/webviewx.dart';
+import 'package:dio/dio.dart';
 
+import '../classes/substitution_entry.dart';
 import '../widgets/app_bar.dart';
 import '../widgets/drawer.dart';
 
 class SubstitutionsScreen extends StatefulWidget {
-  const SubstitutionsScreen({Key? key})
-      : super(key: key);
+  const SubstitutionsScreen({Key? key}) : super(key: key);
 
   @override
   State<SubstitutionsScreen> createState() => _SubstitutionsScreenState();
 }
 
 class _SubstitutionsScreenState extends State<SubstitutionsScreen> {
+  late WebViewXController webviewController;
 
-  final List<String> substitutionURIs = ["", ""];
-  bool isLoading = true;
+  List<Map> substitutionData = [{}, {}, {}];
+  int length = 3;
+  String plan1Name = 'Plan 1';
+  String plan2Name = 'Plan 2';
+  String plan3Name = 'Plan 3';
 
   @override
   void initState() {
+    initializeDateFormatting('de_DE');
     super.initState();
-    _getSubstitutionsURI();
+    _getSubstitutions();
   }
 
-  Future<void> _getSubstitutionsURI() async {
-    final session = Session('b89afef4-8a32-4480-bef8-1c874b1d8e62');
-    final jsonText = await session.getTimetablesJsonString();
-    final data = json.decode(jsonText);
+  void _getSubstitutions() async {
+    var dio = Dio();
+
+    for (int offset = 0; offset < 3; offset++) {
+      DateTime now = DateTime.now();
+      DateFormat format = DateFormat('yyyyMMdd');
+      var response = await dio.post(
+        'https://herakles.webuntis.com/WebUntis/monitor/substitution/data?school=Marie-Curie-Gym',
+        data: {
+          'formatName': 'Vertretungsplan DSB',
+          'schoolName': 'Marie-Curie-Gym',
+          'date': format.format(now),
+          'dateOffset': now.hour < 12 || now.minute == 0 ? offset : offset-1,
+          'strikethrough': false,
+          'mergeBlocks': true,
+          'showOnlyFutureSub': false,
+          'showBreakSupervisions': false,
+          'showTeacher': true,
+          'showClass': true,
+          'showHour': true,
+          'showInfo': true,
+          'showRoom': true,
+          'showSubject': true,
+          'groupBy': 1,
+          'hideAbsent': false,
+          'departmentIds': [],
+          'departmentElementType': -1,
+          'hideCancelWithSubstitution': false,
+          'hideCancelCausedByEvent': false,
+          'showTime': true,
+          'showSubstText': true,
+          'showAbsentElements': [],
+          'showAffectedElements': [],
+          'showUnitTime': false,
+          'showMessages': true,
+          'showStudentgroup': false,
+          'enableSubstitutionFrom': true,
+          'showSubstitutionFrom': 1200,
+          'showTeacherOnEvent': false,
+          'showAbsentTeacher': true,
+          'strikethroughAbsentTeacher': false,
+          'activityTypeIds': [],
+          'showEvent': true,
+          'showCancel': true,
+          'showOnlyCancel': false,
+          'showSubstTypeColor': false,
+          'showExamSupervision': true,
+          'showUnheraldedExams': false
+        },
+      );
+      setState(() {
+        substitutionData.insert(offset, response.data);
+      });
+    }
     setState(() {
-      substitutionURIs.insert(0, data[1]['Childs'][0]['Detail']);
-      substitutionURIs.insert(1, data[1]['Childs'][1]['Detail']);
-      isLoading = false;
+      plan1Name = _getDateFormat(substitutionData[0]['payload']['nextDate'].toString());
+      plan2Name = _getDateFormat(substitutionData[1]['payload']['nextDate'].toString());
+      plan3Name = _getDateFormat(substitutionData[2]['payload']['nextDate'].toString());
     });
+  }
+
+  String _getDateFormat(String date) {
+    String now = DateFormat('yyyyMMdd').format(DateTime.now());
+    int nowInt = int.parse(now);
+    if (date != DateFormat('yyyyMMdd').format(DateTime.parse(date))) return 'Ungültiges Datum';
+    int dateInt = int.parse(date);
+
+    if (date == now) return 'Heute';
+    if (dateInt == nowInt + 1) return 'Morgen';
+    if (dateInt == nowInt - 1) return 'Gestern';
+    if (dateInt > nowInt && dateInt < nowInt + 7) return DateFormat('EEEE', 'de').format(DateTime.parse(date));
+    return DateFormat('dd.MM.yyyy').format(DateTime.parse(date));
   }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       initialIndex: 0,
-      length: 2,
+      length: 3,
       child: Scaffold(
         appBar: MCGAppBar(
           title: 'Vertretungsplan',
@@ -49,46 +117,84 @@ class _SubstitutionsScreenState extends State<SubstitutionsScreen> {
             IconButton(
               icon: const Icon(Icons.refresh),
               onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          const SubstitutionsScreen()),
-                  (Route<dynamic> route) => false,
-                );
+                setState(() {
+                  substitutionData = [{}, {}, {}];
+                  plan1Name = 'Plan 1';
+                  plan2Name = 'Plan 2';
+                  plan3Name = 'Plan 3';
+                });
+                _getSubstitutions();
               },
             ),
             IconButton(
               icon: const Icon(Icons.filter_alt),
-              onPressed: () { },
+              onPressed: () {},
             ),
           ],
-          bottom: const TabBar(
-            physics: NeverScrollableScrollPhysics(),
+          bottom: TabBar(
             tabs: [
               Tab(
-                child: Text("Plan 1"),
+                child: Text(plan1Name),
               ),
               Tab(
-                child: Text("Plan 2"),
+                child: Text(plan2Name),
               ),
+              Tab(
+                child: Text(plan3Name),
+              )
             ],
           ),
         ),
         drawer: const MCGDrawer(),
         body: TabBarView(
           children: [
-            InteractiveViewer(
-              panEnabled: false,
-              child: isLoading
-                  ? const Center(child: Text('Wird geladen...'))
-                  : WebView(initialUrl: substitutionURIs[0]),
+            substitutionData[0].isEmpty
+                ? const Center(child: Text('Wird geladen...'))
+                : ListView.builder(
+                    itemCount: substitutionData[0]['payload']['rows'].length,
+                    itemBuilder: (BuildContext context, int index) {
+                      if (index == 0) {
+                        return Padding(
+                          padding: const EdgeInsets.only(
+                              left: 10.0, top: 5.0, bottom: 5.0),
+                          child: Text('Stand: ${substitutionData[0]['payload']['lastUpdate']}'),
+                        );
+                      }
+                      var data = substitutionData[0]['payload']['rows'][index - 1];
+                      return SubstitutionEntry.fromJson(data);
+                    },
+                  ),
+            substitutionData[1].isEmpty
+                ? const Center(child: Text('Wird geladen...'))
+                : ListView.builder(
+              itemCount: substitutionData[1]['payload']['rows'].length,
+              itemBuilder: (BuildContext context, int index) {
+                if (index == 0) {
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                        left: 10.0, top: 5.0, bottom: 5.0),
+                    child: Text('Stand: ${substitutionData[1]['payload']['lastUpdate']}'),
+                  );
+                }
+                var data = substitutionData[1]['payload']['rows'][index - 1];
+                return SubstitutionEntry.fromJson(data);
+              },
             ),
-            InteractiveViewer(
-              panEnabled: false,
-              child: isLoading
-                  ? const Center(child: Text('Wird geladen...'))
-                  : WebView(initialUrl: substitutionURIs[1]),
+            substitutionData[2].isEmpty
+                ? const Center(child: Text('Wird geladen...'))
+                : ListView.builder(
+              itemCount: substitutionData[2]['payload']['rows'].length,
+              itemBuilder: (BuildContext context, int index) {
+                if (index == 0) {
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                        left: 10.0, top: 5.0, bottom: 5.0),
+                    child: Text('Stand: ${substitutionData[2]['payload']['lastUpdate']}'),
+                  );
+                }
+                var data = substitutionData[2]['payload']['rows'][index - 1];
+                return SubstitutionEntry.fromJson(data);
+              },
             ),
           ],
         ),
