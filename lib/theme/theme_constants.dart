a3f62e417aecb9950affd4a449df1c6eb2ec4f52
SLH335 <114884726+SLH335@users.noreply.github.com>
2022-10-31 13:30:39 +0100

    Remove unused colors from palette

--- lib/theme/theme_constants.dart
+++ lib/theme/theme_constants.dart
@@ -6,9 +6,6 @@ Color colorPrimaryDark = Colors.green.shade900;
 Color colorAccentLight = Colors.greenAccent.shade700;
 Color colorAccentDark = Colors.lightGreen.shade700;
 
-Color colorBackgroundLight = Colors.lightGreenAccent.shade100;
-Color colorBackgroundDark = Colors.greenAccent.shade400;
-
 Color colorStrokeLight = Colors.black;
 Color colorStrokeDark = Colors.white;
 
@@ -18,7 +15,6 @@ Color colorSecondaryDark = Colors.green.shade800;
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
   primaryColor: colorPrimaryLight,
-  backgroundColor: colorBackgroundLight,
   appBarTheme: AppBarTheme(
     backgroundColor: colorPrimaryLight,
   ),
@@ -30,7 +26,6 @@ ThemeData lightTheme = ThemeData(
 ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
   primaryColor: colorPrimaryDark,
-  backgroundColor: colorBackgroundDark,
   appBarTheme: AppBarTheme(
     backgroundColor: colorPrimaryDark,
   ),
