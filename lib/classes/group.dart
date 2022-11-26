ee7d1dad21e411bc09445fa7be23c6c1e115e1aa
SLH335 <114884726+SLH335@users.noreply.github.com>
2022-11-26 22:18:26 +0100

    Add missing groups and disable groups that do not have courses yet

--- lib/classes/group.dart
+++ lib/classes/group.dart
@@ -1,5 +1,5 @@
 enum Group {
-  class7a('7a', 7),
+  /*class7a('7a', 7),
   class7b('7b', 7),
   class7c('7c', 7),
   class7d('7d', 7),
@@ -11,23 +11,25 @@ enum Group {
   class8d('8d', 8),
   class8e('8e', 8),
   class9a('9a', 9),
-  class9b('9b', 9),
+  class9b('9b', 9),*/
   class9c('9c', 9),
-  class9d('9d', 9),
+  /*class9d('9d', 9),
+  class9e('9e', 9),
   class10a('10a', 10),
   class10b('10b', 10),
   class10c('10c', 10),
   class10d('10d', 10),
+  class10e('10e', 10),*/
   class11_1('11-1', 11),
   class11_2('11-2', 11),
   class11_3('11-3', 11),
   class11_4('11-4', 11),
-  class11_5('11-5', 11),
+  class11_5('11-5', 11)/*,
   class12_1('12-1', 12),
   class12_2('12-2', 12),
   class12_3('12-3', 12),
   class12_4('12-4', 12),
-  class12_5('12-5', 12);
+  class12_5('12-5', 12)*/;
 
   const Group(this.name, this.level);
   final String name;
